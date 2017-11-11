b93a1c3b460d96bc355e733b45c97f41a8b5f919
Stefanie Brabec <sbrabec@me.com>
2017-11-11 02:12:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function happyHolidays() {
   return ('Happy holidays!')
 }
-function happyHolidaysTo(name) {
-  return ('Happy holidays, ${name}!')
+function happyHolidaysTo(you) {
+  return ('Happy holidays, you!')
 }
