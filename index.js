13a99a0c64ab91c04e00fe0ab29aff06b2dcceef
Stefanie Brabec <sbrabec@me.com>
2017-11-11 02:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(holiday,name) {
   return ('Happy Independence Day, you!')
 }
 function holidayCountdown(holiday,days) {
-  return ("Mother's Day", 20)
+  if("Mother's Day", 20) return('It\'s 20 days until Mother\'s Day')
 }
